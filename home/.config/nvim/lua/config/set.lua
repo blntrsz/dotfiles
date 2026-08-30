ed100ae68ee6d87fb58b2cc3a45b3c2540ade92d
Balint Orosz <orosz.balint.97@gmail.com>
2026-08-30 10:55:46 +0200

    nvim: migrate nvim-treesitter to main branch API
    - Use new nvim.treesitter.start FileType autocmd instead of configs.setup
    - Install parsers explicitly via nvim-treesitter.install
    - Drop legacy treesitter foldexpr options
    - Pin nvim-treesitter to main branch in lazy-lock
    - pi: add glm-5.3-flash to default models

--- home/.config/nvim/lua/config/set.lua
+++ home/.config/nvim/lua/config/set.lua
@@ -36,8 +36,6 @@ vim.opt.wrap = true
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldenable = false
 
 vim.cmd([[autocmd BufReadPost,FileReadPost * normal zR]])
--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,10 @@
+local parsers = { 'javascript', 'typescript', 'tsx', 'css', 'html', 'markdown', 'markdown_inline' }
+local filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'css', 'html', 'markdown' }
+
+local function start_treesitter(buf)
+  pcall(vim.treesitter.start, buf)
+end
+
 return {
   {
     'JoosepAlviste/nvim-ts-context-commentstring',
@@ -7,14 +14,19 @@ return {
   },
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = filetypes,
+        callback = function(args)
+          start_treesitter(args.buf)
+        end,
+      })
+    end,
     config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { "javascript", "typescript", "tsx", "css", "html" },
-        highlight = {
-          enable = true,
-        },
-      }
+      require('nvim-treesitter').install(parsers)
     end,
   },
   {
